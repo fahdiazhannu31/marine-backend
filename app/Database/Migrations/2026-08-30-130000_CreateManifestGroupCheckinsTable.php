789c08f3a850c3663ff5426979518ba02a020558
fahdi azhannu <fahdiazhannu31@gmail.com>
2026-08-30 13:01:31 +0700

    feat: add manifest_group_checkins table for tracking group QR check-ins per day

---
+++ app/Database/Migrations/2026-08-30-130000_CreateManifestGroupCheckinsTable.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateManifestGroupCheckinsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->createTable('manifest_group_checkins', [
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'auto_increment' => true,
+                'unsigned'       => true,
+            ],
+            'upload_id' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+            ],
+            'group_name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'checked_in_at' => [
+                'type' => 'DATETIME',
+            ],
+            'direction' => [
+                'type'       => 'ENUM',
+                'constraint' => ['DEPARTURE', 'RETURN'],
+                'default'    => 'DEPARTURE',
+            ],
+            'notes' => [
+                'type'       => 'TEXT',
+                'null'       => true,
+            ],
+        ]);
+
+        $this->forge->addPrimaryKey('id', 'manifest_group_checkins');
+        $this->forge->addKey(['upload_id', 'group_name'], 'manifest_group_checkins');
+        $this->forge->addKey('checked_in_at', 'manifest_group_checkins');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('manifest_group_checkins');
+    }
+}
